7d6e1a43f74e722f487ab963a6c8b99082b141a9
HeitorMOliveira <heitorm0rei1@gmail.com>
2020-07-06 12:56:47 -0300

    Product & Many-to-many association with JoinTable

--- src/main/java/com/example/course/entities/Product.java
+++ src/main/java/com/example/course/entities/Product.java
@@ -8,30 +8,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-@Entity(name = "tb_category")
-public class Category implements Serializable {
-	
-
+@Entity(name = "tb_product")
+public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	@JsonIgnore
-	@ManyToMany(mappedBy = "categories")
-	private Set<Product> products = new HashSet<>();
+	private String description;
+	private double price;
+	private String imgUrl;
 	
-	public Category(Long id, String name) {
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id")) 
+	private Set<Category> categories = new HashSet<>();
+
+	public Product(Long id, String name, String description, double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
-	
-	public Category() {}
+
+	public Product() {
+	}
 
 	public Long getId() {
 		return id;
@@ -48,8 +56,32 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	public Set<Category> getCategories() {
+		return categories;
 	}
 
 	@Override
@@ -68,7 +100,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -77,6 +109,5 @@ public class Category implements Serializable {
 		return true;
 	}
 
-	
-	
+
 }
