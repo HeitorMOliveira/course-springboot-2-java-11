e3bc3980313c0666ab584a592b1fc4d5366e0f32
HeitorMOliveira <heitorm0rei1@gmail.com>
2020-07-08 11:32:47 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -97,6 +97,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for(OrderItem o: items) {
+			sum = sum + o.getSubTotal();
+		}
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
